04501d89f63ba3c139868fed50d51ffe0707d5c8
Adegbokun Adefela <felaadegbokun@gmail.com>
2025-09-10 02:28:03 +0100

    fix: add correct imports to Route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header.tsx";
 import Homepage from "./components/Homepage";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
