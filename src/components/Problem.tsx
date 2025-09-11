847b39791fa56e297782a4844dd499a8ec37e2d3
Adegbokun Adefela <felaadegbokun@gmail.com>
2025-09-11 03:57:29 +0100

    feat: add Problem component

---
+++ src/components/Problem.tsx
@@ -0,0 +1,21 @@
+function Problem() {
+  return (
+    <section className="py-16 lg:py-24">
+      <div className="container mx-auto px-4 lg:px-8">
+        <div className="max-w-6xl mx-auto">
+          <div className="text-center mb-12 lg:mb-16 space-y-4">
+            <h2 className="text-black font-Poppins font-medium text-5xl lg:text-6xl tracking-tight">
+              Laptop shopping shouldn't feel like rocket science.
+            </h2>
+            <p className="text-gray-600 font-Be-Vietnam-Pro text-lg lg:text-[22px]">
+              We get it. Finding the right laptop has become impossibly
+              complicated.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Problem;
