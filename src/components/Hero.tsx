25de4ba8160196181ae2bb16b6973f60ea9e933c
Adegbokun Adefela <felaadegbokun@gmail.com>
2025-09-13 18:07:05 +0100

    fix: make scroll indicator visible only on desktop

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -154,7 +154,7 @@ function Hero() {
           </div>
         </div>
         {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <div className="hidden lg:block absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-gray-300 rounded-full flex justify-center">
             <div className="w-1 h-3 bg-gray-400 rounded-full mt-2 animate-pulse"></div>
           </div>
