c501271edb60d206fe96eb914f3767cf80688154
Adegbokun Adefela <felaadegbokun@gmail.com>
2026-01-29 21:04:02 +0100

    remove chakraUI Provider and deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { Provider } from "./components/ui/provider.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -8,9 +7,7 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider>
         <App />
-      </Provider>
     </BrowserRouter>
   </StrictMode>,
 );
