ec21657390521307b53834263ad0bbf797e1ed29
Adegbokun Adefela <felaadegbokun@gmail.com>
2026-01-29 13:13:39 +0100

    feat: add route to company page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header.tsx";
+import Company from "./components/Company.tsx";
 import Homepage from "./components/Homepage";
 // import Footer from "./components/Footer";
 
@@ -9,8 +10,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/company" element={<Company />} />
       </Routes>
-      {/* <Footer /> */}
     </>
   );
 }
